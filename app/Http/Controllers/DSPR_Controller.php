9664f2eb1aa8c44c24061fa3cba903e88f9449f7
ShahdAboLatif <shahdabolatif@gmail.com>
2025-09-17 13:55:20 -0400

    re build the dspr controller

--- app/Http/Controllers/DSPR_Controller.php
+++ app/Http/Controllers/DSPR_Controller.php
@@ -10,7 +10,7 @@ use Carbon\CarbonInterface;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Collection;
-
+use Carbon\CarbonImmutable;
 class DSPR_Controller extends Controller
 {
     public function index($store, $date, $items = null)
@@ -23,14 +23,120 @@ class DSPR_Controller extends Controller
             return response()->json(['error' => 'Invalid date format, expected YYYY-MM-DD or YYYY-M-D'], 400);
         }
 
-        // URL decode the items parameter
-        $decodedItems = $items ? urldecode($items) : null;
-        // $itemsArray = $decodedItems ? $this->parseItemsString($decodedItems) : [];
+        /***dates Used**/
+        $givenDate = Carbon::parse($date);
+        $usedDate = CarbonImmutable::parse($givenDate);
+        //for week
+        $weekNumber = $usedDate->isoWeek;
+        $weekStartDate = $usedDate->startOfWeek(Carbon::TUESDAY);
+        $weekEndDate = $weekStartDate->addDays(6);
+        //for lookback
+        $lookBackStartDate=$usedDate->subDays(84);
+        $lookBackEndDate=$usedDate;
 
-        // External deposit/delivery
+        //items parameter
+        $decodedItems = $items ? urldecode($items) : null;
+        $Itemslist = explode(',', $decodedItems);
+
+
+        // External deposit/delivery data
         $base = rtrim('https://hook.pneunited.com/api/deposit-delivery-dsqr', '/');
         $url  = $base.'/'.rawurlencode($store).'/'.rawurlencode($date);
 
+        // Make the GET request
+        $response = Http::get($url);
+
+        if ($response->successful()) {
+            // Decode the JSON response into a PHP array
+            $data = $response->json();
+
+            // Convert the array into a Laravel collection
+            $depositDeliveryCollection = collect($data['weeklyDepositDelivery']);
+
+        } else {
+            $depositDeliveryCollection = collect();
+        }
+
+
+
+        /**get the data from models as collections**/
+        //daily
+        $dailyFinalSummaryCollection = FinalSummary::
+            where('franchise_store', '=',$store )
+            ->where('business_date','=',$usedDate)
+            ->get();
+
+        $dailySummaryItemCollection = SummaryItem::
+            where('franchise_store', '=',$store )
+            ->where('business_date','=',$usedDate)
+            ->whereIn('menu_item_name',$Itemslist)
+            ->get();
+
+        $dailyHourlySalesCollection = HourlySales::
+        where('franchise_store', '=',$store )
+        ->where('business_date','=',$usedDate)
+        ->get();
+
+        //weekly
+        $weeklyFinalSummaryCollection = FinalSummary::
+            where('franchise_store', '=',$store )
+            ->whereBetween('business_date', [$weekStartDate, $weekEndDate])
+            ->get();
+
+        $weeklySummaryItemCollection = SummaryItem::
+            where('franchise_store', '=',$store )
+            ->whereBetween('business_date', [$weekStartDate, $weekEndDate])
+            ->whereIn('menu_item_name',$Itemslist)
+            ->get();
+
+        $weeklyHourlySalesCollection = HourlySales::
+        where('franchise_store', '=',$store )
+        ->whereBetween('business_date', [$weekStartDate, $weekEndDate])
+        ->get();
+
+        //lookback
+        $lookBackFinalSummaryCollection = FinalSummary::
+            where('franchise_store', '=',$store )
+            ->whereBetween('business_date', [$lookBackStartDate, $lookBackEndDate])
+            ->get();
+
+        $lookBackSummaryItemCollection = SummaryItem::
+            where('franchise_store', '=',$store )
+            ->whereBetween('business_date', [$lookBackStartDate, $lookBackEndDate])
+            ->whereIn('menu_item_name',$Itemslist)
+            ->get();
+
+        $lookBackHourlySalesCollection = HourlySales::
+        where('franchise_store', '=',$store )
+        ->whereBetween('business_date', [$lookBackStartDate, $lookBackEndDate])
+        ->get();
+
+
+
+        return [
+            'Filtering Values'=>[
+                'date'=>$date,
+                'store' =>$store,
+                'items'=>$Itemslist,
+                'week' =>$weekNumber,
+                'weekStartDate' =>$weekStartDate,
+                'weekEndDate'=>$weekEndDate,
+
+                'look back start'=>$lookBackStartDate,
+                'look back end'=>$lookBackEndDate,
+                'depositDeliveryUrl' =>$url,
+                ],
+            'collections'=>[
+                'daily'=>[
+                    'depositDeliveryCollection' =>$depositDeliveryCollection,
+                    'dailyFinalSummaryCollection'=>$dailyFinalSummaryCollection,
+                    'dailySummaryItemCollection'=>$dailySummaryItemCollection,
+                    'dailyHourlySalesCollection' =>$dailyHourlySalesCollection
+                    ]
+            ],
+
+
+        ];
     }
 
     /**
