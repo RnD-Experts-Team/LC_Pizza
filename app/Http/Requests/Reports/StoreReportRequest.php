8aa3cc688454201ea36b89e2f80cc6f2a7002eb8
Aws12505 <awsaljaramani12505@gmail.com>
2025-11-17 08:47:38 -0500

    ..

--- app/Http/Requests/Reports/StoreReportRequest.php
+++ app/Http/Requests/Reports/StoreReportRequest.php
@@ -17,7 +17,7 @@ class StoreReportRequest extends FormRequest
             'franchise_store' => ['nullable','string','max:20'],
             'from'            => ['required','date','before_or_equal:to'],
             'to'              => ['required','date','after_or_equal:from'],
-            'without_bundle'  => ['sometimes','boolean'], // <--- NEW
+            'without_bundle'  => ['sometimes'], // <--- NEW
         ];
     }
 
